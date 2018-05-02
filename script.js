926fc9b48eb08ac92c203ccffffdaf063b8f8798
Sean Carey <seancarey18@gmail.com>
2018-05-02 13:22:58 -0400

    Added functionality to change the grid dimensions based on user input.

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 const screen = document.querySelector('.screen');
+const input = document.querySelector('#grid-size');
+const button = document.querySelector('#button');
 
-for (let i = 0; i < (64*64); i++) {
-	const square = document.createElement('div');
-	square.classList.add('square');
-	square.addEventListener('mouseover', function() {
-		square.style.backgroundColor = 'black';
-	});
-	screen.appendChild(square);
+function createGrid(size) {
+	size = parseInt(size);
+	
+	if (size <= 1) {size = 1;} 
+	else if (size >= 300) {size = 300;}
+	else if (isNaN(size)) {size = 64;}
+
+	while (screen.firstChild) {
+		screen.removeChild(screen.firstChild);
+	}
+
+	screen.style.gridTemplate = `repeat(${size}, 1fr) / repeat(${size}, 1fr)`
+
+	for (let i = 0; i < (size**2); i++) {
+		const square = document.createElement('div');
+		square.classList.add('square');
+		square.addEventListener('mouseover', function() {
+			square.style.backgroundColor = 'black';
+		});
+		screen.appendChild(square);
+	}
 }
+
+createGrid(64);
+
+button.addEventListener('click', () => createGrid(input.value));
